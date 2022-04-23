1994ae0ed330f9a3aa81582cd55890a404bd56d2
Quentin Glorieux <quentin@rubidiumweb.fr>
2022-04-23 02:27:30 +0200

    commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <Checkpoint/>
+</template>
+
+<script>
+import Checkpoint from '~/components/Checkpoint.vue'
+export default {
+  components: { Checkpoint },
+  name: 'IndexPage'
+}
+</script>
---
+++ pages/p1.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  <h1 class="bg-indigo-100 text-xl"> BALISE 1 </h1>
+  <Checkpoint/>
+  </div>
+</template>
+
+<script>
+import Checkpoint from '~/components/Checkpoint.vue'
+export default {
+  components: { Checkpoint },
+  name: 'IndexPage'
+}
+</script>
