c872c60388204844880a509a25a8b1c7bbea8586
Quentin Glorieux <quentin@rubidiumweb.fr>
2022-04-23 03:19:59 +0200

    B

---
+++ pages/p8B_fdaofo.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  <CheckpointA/>
+  </div>
+</template>
+
+<script>
+import CheckpointA from '~/components/Checkpoint8B.vue'
+export default {
+  components: { CheckpointA },
+  name: 'IndexPage'
+}
+</script>
---
+++ pages/p9B_kdfaklkd.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  <CheckpointA/>
+  </div>
+</template>
+
+<script>
+import CheckpointA from '~/components/Checkpoint9B.vue'
+export default {
+  components: { CheckpointA },
+  name: 'IndexPage'
+}
+</script>
